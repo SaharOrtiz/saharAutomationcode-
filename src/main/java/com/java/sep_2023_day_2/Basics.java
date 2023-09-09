5a0463c3617a58eb00dc158b3e0613c7c9141ae4
SaharOrtiz <criseydarugamas@gmail.com>
2023-09-09 18:05:20 -0400

    Pushing code for the first time

---
+++ src/main/java/com/java/sep_2023_day_2/Basics.java
@@ -0,0 +1,14 @@
+package com.java.sep_2023_day_2;
+
+public class Basics {
+
+	public static void main(String[] args) { 
+		int salesep2023 = 200;
+	int salessep2022 = 200;
+	int totalsales = salesep2023 + salessep2022;
+		System.out.println( totalsales);
+	}
+
+	
+
+}
---
+++ src/main/java/com/java/sep_2023_day_2/java_Sahar.java
@@ -0,0 +1,9 @@
+package com.java.sep_2023_day_2;
+
+public class java_Sahar {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
